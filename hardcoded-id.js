3826d2bba28a6e53169ea4972b65e524177b78f3
Lorenzo Frattini <lorenzo.frattini@gmail.com>
2017-04-13 15:57:56 +0100

    updates

---
+++ hardcoded-id.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var myFunction = function(someId) {
+	if (someId === '0FM30000000014C') {
+		return true;
+	}
+}
+var someOtherId = "00D58000000IWk6";
+myFunction('00D58000000IWk6')
